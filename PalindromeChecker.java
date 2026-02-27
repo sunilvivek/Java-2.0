b57d8456879d4127a9dd9c036fc32ffd3fa404e5
vivek <viveks@viveks-MacBook-Air-2.local>
2026-02-27 09:45:37 +0530

    changes made to usecase1

---
+++ PalindromeChecker.java
@@ -0,0 +1,18 @@
+public class PalindromeChecker {
+
+    // Application constants
+    static final String APP_NAME = "Palindrome Checker";
+    static final String VERSION = "1.0.0";
+
+    public static void main(String[] args) {
+
+        // Welcome Message
+        System.out.println("=================================");
+        System.out.println("Welcome to " + APP_NAME);
+        System.out.println("Version: " + VERSION);
+        System.out.println("=================================");
+
+        // Continue to next use case
+        System.out.println("Application is ready...");
+    }
+}
