e2d0b7ea612ee71a20f99527706f9af6f083c7de
vivek <viveks@viveks-MacBook-Air-2.local>
2026-02-27 09:58:22 +0530

    changes made to usecase2

--- PalindromeChecker.java
+++ PalindromeChecker.java
@@ -1,18 +1,24 @@
 public class PalindromeChecker {
 
-    // Application constants
-    static final String APP_NAME = "Palindrome Checker";
-    static final String VERSION = "1.0.0";
-
     public static void main(String[] args) {
 
-        // Welcome Message
-        System.out.println("=================================");
-        System.out.println("Welcome to " + APP_NAME);
-        System.out.println("Version: " + VERSION);
-        System.out.println("=================================");
+        // Hardcoded string
+        String word = "madam";
 
-        // Continue to next use case
-        System.out.println("Application is ready...");
+        // Reverse the string
+        String reversed = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed += word.charAt(i);
+        }
+
+        // Check palindrome using if-else
+        if (word.equals(reversed)) {
+            System.out.println(word + " is a Palindrome.");
+        } else {
+            System.out.println(word + " is NOT a Palindrome.");
+        }
+
+        System.out.println("Program exited successfully.");
     }
 }
