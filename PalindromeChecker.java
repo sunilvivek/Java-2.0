dd4f6769de4da5177603afd52d66014c413a87df
vivek <viveks@viveks-MacBook-Air-2.local>
2026-02-27 10:05:25 +0530

    changes made to usecase3

--- PalindromeChecker.java
+++ PalindromeChecker.java
@@ -1,22 +1,23 @@
-public class PalindromeChecker {
+public class PalindromeChecker{
 
     public static void main(String[] args) {
 
-        // Hardcoded string
-        String word = "madam";
+        // Original String
+        String original = "level";
 
-        // Reverse the string
+        // Variable to store reversed string
         String reversed = "";
 
-        for (int i = word.length() - 1; i >= 0; i--) {
-            reversed += word.charAt(i);
+        // Reverse string using for loop
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
         }
 
-        // Check palindrome using if-else
-        if (word.equals(reversed)) {
-            System.out.println(word + " is a Palindrome.");
+        // Compare original and reversed string
+        if (original.equals(reversed)) {
+            System.out.println(original + " is a Palindrome.");
         } else {
-            System.out.println(word + " is NOT a Palindrome.");
+            System.out.println(original + " is NOT a Palindrome.");
         }
 
         System.out.println("Program exited successfully.");
